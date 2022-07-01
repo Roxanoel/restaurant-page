f12023202a9ae156c63a70bf1e67cf610fa7b60b
Roxanoel <roxane.noel92@gmail.com>
2022-07-01 12:38:08 +0100

    index.js now generates the shared elements. Those are the elements present for each page, like the header and link tabs. I have not yet removed them from index.html because I am still not done with the home page contents.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 import './style.css';
+//#region Global State
+const mainContainer = document.getElementById('content');
+const restaurantName = "The Goblin's Den";
+const linksText = ['The Den', 'Feast!', 'Contact'];
+//#endregion
+
+//#region Execution
+renderSharedContent();
+//#endregion
+
+//#region Methods
+
+// Generates the contents that are repeated for each page. 
+function renderSharedContent() {
+    mainContainer.appendChild(generateHeader());
+    mainContainer.appendChild(generateMain());
+}
+
+// Generates the header; to be called inside generateSharedContent, for readability.
+function generateHeader() {
+    const header = document.createElement('header');
+
+    // heading 1
+    const h1 = document.createElement('h1');
+    h1.textContent = restaurantName;
+
+    // link tabs
+    const linkTabs = generateLinkTabs();
+
+    //appending children
+    header.appendChild(h1);
+    header.appendChild(linkTabs);
+
+    return header;
+    
+}
+
+function generateLinkTabs() {
+    // ul
+    const linksContainer = document.createElement('ul');
+    linksContainer.classList.add('links-container');
+    // links
+    const links = [];
+    for (let i = 0; i < 3; i++) {
+        const link = document.createElement('li');
+        link.textContent = linksText[i];
+        links.push(link);
+    }
+    // add links to ul 
+    links.forEach(link => linksContainer.appendChild(link));
+
+    return linksContainer;
+}
+
+function generateMain() {
+    const main = document.createElement('main');
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('main-container');
+    main.appendChild(mainContainer);
+    return main;
+}
+
+//#endregion
