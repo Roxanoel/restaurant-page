9866ec6f580e1bf75df1b114c7ac244bfd0d757b
Roxanoel <roxane.noel92@gmail.com>
2022-07-02 11:03:26 +0100

    Generates elements for contact page. No styling yet.

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,7 @@
 //#region State
-const headingText = 'Where to find us'
+const headingText = 'Where to find us';
+const addressText = 'Main Square, Queensport';
+const openingTimesText = 'Open every day from 5pm until the break of dawn!'
 //#endregion
 
 //#region functions
@@ -14,13 +16,66 @@ function _generateMapContainer() {
     const heading = document.createElement('h2');
     heading.textContent = headingText;
     
-    const mapInfoGrid = document.createElement('div');
-    mapInfoGrid.classList.add('map-info-grid');
+    const mapInfoGrid = _generateMapInfoGrid();
 
+    // Appending children
     mapContainer.appendChild(heading);
     mapContainer.appendChild(mapInfoGrid);
 
     return mapContainer;
+}
+
+function _generateMapInfoGrid() {
+    const mapInfoGrid = document.createElement('div');
+    mapInfoGrid.classList.add('map-info-grid');
+
+    const mapImg = document.createElement('div');
+    mapImg.setAttribute('id', 'map-img');
+
+    const address = _generateAddressDiv();
+
+    const openingTimes = _generateOpeningTimesDiv();
+
+    // Appending children
+    mapInfoGrid.appendChild(mapImg);
+    mapInfoGrid.appendChild(address);
+    mapInfoGrid.appendChild(openingTimes);
+
+    return mapInfoGrid;
+}
+
+function _generateAddressDiv() {
+    const div = document.createElement('div')
+    div.classList.add('address');
+
+    const heading = document.createElement('h3');
+    heading.textContent = 'Address:';
+
+    const address = document.createElement('p');
+    address.textContent = addressText;
+
+    // Appending children
+    div.appendChild(heading);
+    div.appendChild(address);
+
+    return div;
+}
+
+function _generateOpeningTimesDiv() {
+    const div = document.createElement('div')
+    div.classList.add('opening-times');
+
+    const heading = document.createElement('h3');
+    heading.textContent = 'Opening times:';
+
+    const times = document.createElement('p');
+    times.textContent = openingTimesText;
+
+    // Appending children
+    div.appendChild(heading);
+    div.appendChild(times);
+
+    return div;
 }
 //#endregion
 
